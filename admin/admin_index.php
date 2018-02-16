a7a5d9e8358696914d66f2a98714dac69601b3bb
elee378 <ericjaelee@hotmail.com>
2018-02-16 15:33:51 -0500

    Update admin_index.php

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -12,13 +12,13 @@
 </head>
 <body>
 	<?php
-	  	if($_SESSION['current_hour'] <= "11") { //If current_hour is <= then display Good morning
+	  	if($_SESSION['current_hour'] <= "11") {
 	    	$greeting = "Good morning ";
 
-	  	}elseif($_SESSION['current_hour'] >= "12" && $_SESSION['current_hour'] <= "16") { //If current_hour is between 12 & 16, display Good afternoon
+	  	}elseif($_SESSION['current_hour'] >= "12" && $_SESSION['current_hour'] <= "16") { 
 	   		$greeting = "Good afternoon ";
 	   		
-	  	}elseif($_SESSION['current_hour'] >= "17") { // if current_hour is >= 17 display good evening
+	  	}elseif($_SESSION['current_hour'] >= "17") { 
 	    	$greeting = "Good evening ";
 	  	}
 	?>
