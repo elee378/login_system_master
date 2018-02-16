c6eafac2277b763e77f5720b528d672753ab9f02
elee378 <ericjaelee@hotmail.com>
2018-02-15 20:27:50 -0500

    second commit
    added functionality: date time, account lockout, message.

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -11,25 +11,40 @@
  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
 <body>
-	<?php echo $_SESSION['user_name'];?>
-	<div>
 	<?php
-	if (isset($_SESSION['user_id'])) {
-		echo '<form action="phpscripts/logout_function.php" method="POST">
-			<button type="submit" name="submit">Logout</button>
-			</form>';
-	} else {
-		echo '<form action="admin_login.php" method="post">
-			<label>Username:</label>
-			<input type="text" name="username" value="">
-			<br>
-			<label>Password</label>
-			<input type="password" name="password" value="">
-			<br><br>
-			<input type="submit" name="submit" value="Show me the money">
-			</form>';
-	}
+	  	if($_SESSION['current_hour'] <= "11") { //If current_hour is <= then display Good morning
+	    	$greeting = "Good morning ";
+
+	  	}elseif($_SESSION['current_hour'] >= "12" && $_SESSION['current_hour'] <= "16") { //If current_hour is between 12 & 16, display Good afternoon
+	   		$greeting = "Good afternoon ";
+	   		
+	  	}elseif($_SESSION['current_hour'] >= "17") { // if current_hour is >= 17 display good evening
+	    	$greeting = "Good evening ";
+	  	}
 	?>
+	<div>
+		<?php
+		if (isset($_SESSION['user_id'])) {
+			echo '<form action="phpscripts/logout_function.php" method="POST">
+				<button type="submit" name="submit">Logout</button>
+				</form>';
+		} else {
+			echo '<form action="admin_login.php" method="post">
+				<label>Username:</label>
+				<input type="text" name="username" value="">
+				<br>
+				<label>Password</label>
+				<input type="password" name="password" value="">
+				<br><br>
+				<input type="submit" name="submit" value="Show me the money">
+				</form>';
+		}
+		?>
+
+		<?php echo $_SESSION['user_name'];?>
+		<?php echo $greeting?>
 	</div>
+
+	<p id="lastLogin">You last logged in on <?php echo $_SESSION['users_timestamp']; ?>.</p>
 </body>
 </html>
--- admin/phpscripts/login.php
+++ admin/phpscripts/login.php
@@ -1,5 +1,4 @@
 <?php
-
 	function logIn($username, $password, $ip) {
 		require_once('connect.php');
 		$username = mysqli_real_escape_string($link, $username);
@@ -8,16 +7,52 @@
 		$loginstring = "SELECT * FROM tbl_user WHERE user_name='{$username}' AND user_pass='{$password}'";
 		$user_set = mysqli_query($link, $loginstring);
 		//echo mysqli_num_rows($user_set);
+
+	//THIS IS THE LOCKOUT FUNCTION
+	$lockoutstring = "SELECT * FROM tbl_user WHERE user_name = '$username' OR user_pass = '$password'"; 
+
+		$result = mysqli_query($link, $lockoutstring); 
+		$foundQuery = mysqli_num_rows($result); 
+
+		if($foundQuery < 2) { 
+			$updateLogin = "UPDATE tbl_user SET user_loginAttempts = user_loginAttempts + 1 WHERE user_ip = '$ip'";
+			$updateLoginQuery = mysqli_query($link, $updateLogin);
+
+			$attemptsString = "SELECT user_loginAttempts FROM tbl_user WHERE user_ip ='$ip' AND user_loginAttempts >= '3'";
+			$attempts = mysqli_query($link, $attemptsString);
+			$threeAttempts = mysqli_num_rows($attempts);
+
+			if($threeAttempts == "1") { 
+				echo 'FAILED 3 TIMES.';
+			}
+
+		}elseif($foundQuery = 2) {
+			$updateLogin = "UPDATE tbl_user SET user_loginAttempts = '0' WHERE user_ip = '$ip'";
+			$updateLoginQuery = mysqli_query($link, $updateLogin);
+		}
+		
 		if(mysqli_num_rows($user_set)){
 			$founduser = mysqli_fetch_array($user_set, MYSQLI_ASSOC);
 			$id = $founduser['user_id'];
 			$_SESSION['user_id'] = $id;
 			$_SESSION['user_name']= $founduser['user_fname'];
-			if(mysqli_query($link, $loginstring)){
-				$update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
-				$updatequery = mysqli_query($link, $update);
+				if(mysqli_query($link, $loginstring)){
+					//THIS UPDATES THE DATABASE
+					$update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
+					$updatequery = mysqli_query($link, $update);
+
+					date_default_timezone_set('America/Toronto');
+					$currentDate = date('l F jS Y, \a\t h:ia T'); 
+					$_SESSION['current_hour'] = date('G');
+					$updateTime = "UPDATE tbl_user SET user_timestamp = '$currentDate' WHERE user_id = {$id}";
+					$updateTimeQuery = mysqli_query($link, $updateTime);
+
+					$timestamp = $found_user['user_timestamp'];
+					$_SESSION['users_timestamp'] = $timestamp; 
 			}
+
 			redirect_to("admin_index.php");
+
 		}else{
 			$message = "Learn how to type you dumba&*.";
 			return $message;
