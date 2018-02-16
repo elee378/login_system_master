e9b01af5b35615e7f152cd26a6fe9e340582ca24
elee378 <ericjaelee@hotmail.com>
2018-02-16 15:33:12 -0500

    Add files via upload

---
+++ db/db_login.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 16, 2018 at 08:33 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `user_id` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
+  `user_fname` varchar(50) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_pass` varchar(50) NOT NULL,
+  `user_level` varchar(25) NOT NULL,
+  `user_ip` varchar(100) NOT NULL,
+  `user_timestamp` varchar(100) NOT NULL,
+  `user_loginAttempts` tinyint(4) NOT NULL,
+  `user_lastLoginAttempt` datetime NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_name`, `user_pass`, `user_level`, `user_ip`, `user_timestamp`, `user_loginAttempts`, `user_lastLoginAttempt`) VALUES
+(1, 'Eric', 'elee378', 'monkey911', '1', '127.0.0.1', 'Friday February 16th 2018, at 03:27pm EST', 5, '0000-00-00 00:00:00'),
+(2, 'Admin', 'Admin', 'Admin', '2', '', '', 0, '0000-00-00 00:00:00');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
