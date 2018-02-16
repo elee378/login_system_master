9cebf9da97963c0f231304b23d04c9da1771afd5
elee378 <ericjaelee@hotmail.com>
2018-02-16 15:37:42 -0500

    Add files via upload

--- db/db_login.sql
+++ db/db_login.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 16, 2018 at 08:33 PM
+-- Generation Time: Feb 16, 2018 at 08:37 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -31,7 +31,6 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `user_fname` varchar(50) NOT NULL,
   `user_name` varchar(50) NOT NULL,
   `user_pass` varchar(50) NOT NULL,
-  `user_level` varchar(25) NOT NULL,
   `user_ip` varchar(100) NOT NULL,
   `user_timestamp` varchar(100) NOT NULL,
   `user_loginAttempts` tinyint(4) NOT NULL,
@@ -43,9 +42,9 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_name`, `user_pass`, `user_level`, `user_ip`, `user_timestamp`, `user_loginAttempts`, `user_lastLoginAttempt`) VALUES
-(1, 'Eric', 'elee378', 'monkey911', '1', '127.0.0.1', 'Friday February 16th 2018, at 03:27pm EST', 5, '0000-00-00 00:00:00'),
-(2, 'Admin', 'Admin', 'Admin', '2', '', '', 0, '0000-00-00 00:00:00');
+INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_name`, `user_pass`, `user_ip`, `user_timestamp`, `user_loginAttempts`, `user_lastLoginAttempt`) VALUES
+(1, 'Eric', 'elee378', 'monkey911', '127.0.0.1', 'Friday February 16th 2018, at 03:27pm EST', 7, '0000-00-00 00:00:00'),
+(2, 'Admin', 'Admin', 'Admin', '', '', 0, '0000-00-00 00:00:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
