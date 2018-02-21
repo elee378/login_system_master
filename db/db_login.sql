1cadf19a30f3bcdbc460954aee7e7a9dd86d8f83
elee378 <ericjaelee@hotmail.com>
2018-02-21 12:24:25 -0500

    added email
    added email

--- db/db_login.sql
+++ db/db_login.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Feb 16, 2018 at 08:37 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+-- Host: 127.0.0.1
+-- Generation Time: Feb 21, 2018 at 05:24 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `db_login`
@@ -26,26 +26,46 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_user`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_user` (
-  `user_id` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_user` (
+  `user_id` smallint(4) UNSIGNED NOT NULL,
   `user_fname` varchar(50) NOT NULL,
+  `user_lname` varchar(50) NOT NULL,
   `user_name` varchar(50) NOT NULL,
   `user_pass` varchar(50) NOT NULL,
+  `user_level` varchar(25) NOT NULL,
   `user_ip` varchar(100) NOT NULL,
   `user_timestamp` varchar(100) NOT NULL,
   `user_loginAttempts` tinyint(4) NOT NULL,
   `user_lastLoginAttempt` datetime NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `user_email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_name`, `user_pass`, `user_ip`, `user_timestamp`, `user_loginAttempts`, `user_lastLoginAttempt`) VALUES
-(1, 'Eric', 'elee378', 'monkey911', '127.0.0.1', 'Friday February 16th 2018, at 03:27pm EST', 7, '0000-00-00 00:00:00'),
-(2, 'Admin', 'Admin', 'Admin', '', '', 0, '0000-00-00 00:00:00');
+INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_lname`, `user_name`, `user_pass`, `user_level`, `user_ip`, `user_timestamp`, `user_loginAttempts`, `user_lastLoginAttempt`, `user_email`) VALUES
+(1, 'Eric', 'Lee', 'elee378', 'monkey911', '1', '::1', 'Wednesday February 21st 2018, at 12:19pm EST', 41, '0000-00-00 00:00:00', '');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
